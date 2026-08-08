e1a4dd351fe3797c082ad01c1534e84b937ea19d
madhu2007-offical <madhumithak294@gmail.com>
2026-08-08 10:00:27 +0530

    style: switch leaflet tile layer to Google Maps light roadmap style

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1801,9 +1801,9 @@ export default function App() {
                       <ChangeView center={mapCenter} zoom={mapZoom} />
                       <MapClickEvents onMapClick={handleMapClick} />
                       
-                      <TileLayer
-                        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                       <TileLayer
+                        attribution='&copy; Google Maps'
+                        url="https://mt1.google.com/vt/lyrs=m&x={x}&y={y}&z={z}"
                         maxZoom={19}
                       />
 
